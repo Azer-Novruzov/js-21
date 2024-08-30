218c3465cf596063601a7d44c43c7ba9655194ff
Azer-Novruzov <azernovruzov200@gmail.com>
2024-08-30 12:47:56 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+let stockItem = {
+    id: "5",
+    place: "room-6",
+    weight: 45
+};
+let shopItem = {
+    id: stockItem.id,
+    weight: stockItem.weight,
+    price: 100
+};
+console.log(stockItem);
+console.log(shopItem);
